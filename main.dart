3d430d85cde1f137ba0245a12ffdc92637882726
izzulm <muhizzul16@gmail.com>
2020-03-16 17:12:52 +0700

    pertama

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'dart.io';
+
+void main() {
+
+}
+
+void data {
+  var user1 = "izzul";
+  var pass1 = "123";
+}
+
+void login {
+  if (user1 == "izzul" && pass1 = "123"){
+    bool cek = true;
+  }
+
+  print(cek);
+  
+  
+}
+
+
+
